f1b6f7ce6eae838e241d5d4205afbce2c9d1ba13
Silca93 <eric_delhaye@.com>
2024-02-26 14:16:36 +0100

    adding page switching

--- src/Main/Mains.jsx
+++ src/Main/Mains.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Sidebar from '../Sidebar/Sidebar'
 import YourInfo from '../YourInfo/YourInfo'
 import Plan from '../Plan/Plan'
 
 
 export default function Mains({textMatch, emailMatch, phoneMatch, email, phone, text, arcade, advanced, pro}) {
+
+    const [page, setPage] = useState("YourInfo")
+
+    let clickNext = () => {
+        setPage("Plan")
+    }
+
+
   return (
     <div className="w-dvw h-dvh bg-[#ecf3fd] flex justify-center items-center">
         <div className="w-[50rem] h-[70%] rounded-lg flex">
@@ -12,9 +21,10 @@ export default function Mains({textMatch, emailMatch, phoneMatch, email, phone, 
                 <Sidebar/>
             </div>
             <div className="right w-[35rem] h-full bg-[#fffffe] rounded-r-xl relative flex flex-col pt-8">
-                {/* <YourInfo textMatch={textMatch} emailMatch={emailMatch} phoneMatch={phoneMatch} text={text} phone={phone} email={email}/> */}
-                <Plan arcade={arcade} advanced={advanced} pro={pro} />
-                <button className="nextButton bg-[#04264f] w-[6rem] h-[3rem] rounded-xl absolute bottom-1 right-12 text-white">Next Step</button>
+                {page == "YourInfo" && <YourInfo textMatch={textMatch} emailMatch={emailMatch} phoneMatch={phoneMatch} text={text} phone={phone} email={email} page={page}/>}
+                {page == "Plan" && <Plan arcade={arcade} advanced={advanced} pro={pro} /> }
+                
+                <button onClick={() => clickNext()} className="nextButton bg-[#04264f] w-[6rem] h-[3rem] rounded-xl absolute bottom-1 right-12 text-white">Next Step</button>
 
                
 
--- src/Plan/Plan.jsx
+++ src/Plan/Plan.jsx
@@ -7,30 +7,38 @@ export default function Plan({arcade, advanced, pro}) {
             <h1 className="Title text-[2rem] text-[#04264f]">Select your Plan</h1>
             <p className="text-gray-400 ">You have the option of monthly or yearly billing.</p>
         </div>
-        <div className="field w-full h-[30rem] flex justify-start items-start pl-[4rem]  gap-3">
+        <div className="TEST field w-full h-[10rem] flex justify-start items-start pl-[4rem]  gap-3">
             <div className="name flex flex-col gap-2">
                 
-                <div className="BINGO w-[9rem] h-[10rem] flex flex-col items-start justify-between pl-5 border-solid border-gray-400 border-[1px] rounded-2xl" type="text">
+                <div className="BINGO w-[9rem] h-[9rem] flex flex-col items-start justify-between pl-5 pt-4 border-solid border-gray-400 border-[1px] rounded-2xl active:bg-gray-300 hover:border-purple-600 hover:outline-[3px]" type="text">
                     <img src={arcade} alt=""  />
-                    <h1 className="text-[#04264f]">Arcade<br></br>$9/mo</h1 >
+                    <h1 className="text-[#04264f] pb-2">Arcade<br></br>$9/mo</h1 >
                 </div>    
             
             </div>
             <div className="email flex flex-col gap-2">
-            <div className="BINGO w-[9rem] h-[10rem] flex flex-col items-start justify-between pl-5 border-solid border-gray-400 border-[1px] rounded-2xl" type="text">
+                <div className="BINGO w-[9rem] h-[9rem] flex flex-col items-start justify-between pl-5 pt-4 border-solid border-gray-400 border-[1px] rounded-2xl active:bg-gray-300 hover:border-purple-600 hover:outline-[3px]" type="text">
                     <img src={advanced} alt=""  />
-                    <h1 className="text-[#04264f]">Advanced<br></br>$12/mo</h1>
+                    <h1 className="text-[#04264f] pb-2">Advanced<br></br>$12/mo</h1>
                 </div>  
                 
             </div>
             <div className="phone flex flex-col gap-2">
-            <div className="BINGO w-[9rem] h-[10rem] flex flex-col items-start justify-between pl-5 border-solid border-gray-400 border-[1px] rounded-2xl" type="text">
+                <div className="BINGO w-[9rem] h-[9rem] flex flex-col items-start justify-between pl-5 pt-4 border-solid border-gray-400 border-[1px] rounded-2xl active:bg-gray-300 hover:border-purple-600 hover:outline-[3px]" type="text">
                     <img src={pro} alt=""  />
-                    <h1 className="text-[#04264f]">Pro<br></br>$15/mo</h1>
+                    <h1 className="text-[#04264f] pb-2">Pro<br></br>$15/mo</h1>
+                    
                 </div>  
-                
+            <div className=""></div>    
             </div>
 
+        </div>
+        <div className="w-full h-[5rem]  flex justify-center items-center">
+            <div className="w-5/6 h-4/6 ml-5 bg-[#ecf3fd] flex justify-between items-center px-[4rem]">
+                <p>Monthly</p>
+                <input  type="checkbox" className="toggle bg-black " unchecked />
+                <p className="text-gray-400">Yearly</p>
+            </div>
         </div>
     </div>
   )
